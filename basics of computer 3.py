8de83cda3a04376a9c0f953cfd24a059e8b3a5fb
Abhimishra2812 <101655063+Abhimishra2812@users.noreply.github.com>
2022-03-16 11:27:16 +0530

    Add files via upload

---
+++ basics of computer 3.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().set_next_input('1 )What is an Operating System and what are the types of operating system');get_ipython().run_line_magic('pinfo', 'system')
+Ans:An operating system is a type of software interface between the user and the device hardware.
+    It allows users to communicate with the device and perform the desired functions.
+    Operating systems use two components to manage computer programs and applications.
+    
+get_ipython().set_next_input('2 )What is the Operating system that you use on your computer');get_ipython().run_line_magic('pinfo', 'computer')
+Ans:The operating system that I use on my computer is Microsoft Windows.
+    
+get_ipython().set_next_input('3 )What is the difference between a single user operating system and a multi user operating system');get_ipython().run_line_magic('pinfo', 'system')
+Ans:The diffrence between a single and multi operating system is 
+    1.A Single-User Operating System is a system in which only one user can access the computer system at a time.
+    2.A Multi-User Operating System is a system that allows more than one user to access a computer system at one time.
+    
+4 )What is a multi user operating system? Give examples
+Ans:A multi-user operating system is an operating system
+    that permits several users to access a single system 
+    running to a single operating system.Some examples of a 
+    multi-user OS are Unix, Ubuntu, MacOS, Windows and all Linux
+    based OS.
+    
+5 )What is a Mobile Operating system? Give examples.
+Ans:A mobile operating system (mobile OS) is an OS built exclusively for a mobile device,
+    such as a smartphone, personal digital assistant (PDA), tablet or other embedded mobile OS.
+    Popular mobile operating systems are Android, Symbian, iOS, BlackBerry OS and Windows Mobile.
+    
+get_ipython().set_next_input('6 )Write a step wise process on how to restart a system');get_ipython().run_line_magic('pinfo', 'system')
+Ans:To restart a system of computer here are the following ways:
+    .On your computer keyboard, hold down the control (Ctrl), alternate (Alt), and delete (Del) keys at the same time.
+    .Release the keys and wait for a new menu or window to appear.
+    .In the bottom right corner of the screen, click the Power icon. ...
+    .Select between Shut Down and Restart.
+    
+get_ipython().set_next_input('7 )Write a step wise process on how to Shut down a system');get_ipython().run_line_magic('pinfo', 'system')
+Ans:The proper method to shutdown a system is 
+    Step 1: Click the 'Windows' button at the bottom left-hand corner of your screen.
+    Step 2: Click Shutdown or Restart.
+    Step 3: Wait for the system to power itself down, or start the reboot.
+        
+get_ipython().set_next_input('8 )Write a step wise process on how to put your system to sleep');get_ipython().run_line_magic('pinfo', 'sleep')
+Ans:Following are the steps to put your system to sleep:
+    .Click the Start menu button. The Start menu opens.
+    .Click the triangle icon in the lower right of the Start menu.
+     The Shutdown menu appears to the right of that icon.
+    .Choose the Sleep command from the Shutdown menu. Windows tells your PC to take a nap;
+     it shuts off the lights and the screen goes blank.
+    
+
